30e55efd2d3cf091050f416d86a8ff9a3554d820
amitytobi <amityekoyi@gmail.com>
2026-03-12 08:37:54 +0100

    form actions and submission handling

---
+++ src/components/NewOpinion.jsx
@@ -0,0 +1,94 @@
+import { useActionState, use } from "react";
+import { OpinionsContext } from "../store/opinions-context.jsx";
+import Submit from "./Submit.jsx";
+
+export function NewOpinion() {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function handleShareOpinion(prevState, formData) {
+    const userName = formData.get("userName");
+    const title = formData.get("title");
+    const body = formData.get("body");
+
+    let errors = [];
+
+    if (!userName.trim()) {
+      errors.push("enter a valid name");
+    }
+
+    if (title.trim().length < 5) {
+      errors.push("Title must be at least five characters long");
+    }
+
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push("Opinion must be between 10 and 300 characters long ");
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          userName,
+          title,
+          body,
+        },
+      };
+    }
+
+    await addOpinion({ title, body, userName });
+
+    return { errors: null };
+  }
+
+  const [formState, formAction] = useActionState(handleShareOpinion, {
+    errors: null,
+  });
+
+  return (
+    <div id="new-opinion">
+      <h2>Share your opinion!</h2>
+      <form action={formAction}>
+        <div className="control-row">
+          <p className="control">
+            <label htmlFor="userName">Your Name</label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              defaultValue={formState.enteredValues?.userName}
+            />
+          </p>
+
+          <p className="control">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultValue={formState.enteredValues?.title}
+            />
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="body">Your Opinion</label>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={formState.enteredValues?.body}
+          ></textarea>
+        </p>
+
+        {formState.errors && (
+          <ul className="errors">
+            {formState.errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+
+        <Submit />
+      </form>
+    </div>
+  );
+}
